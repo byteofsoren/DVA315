c6bf99105a9b91de30412a8578eead799c2f5cd6
green4free <ninjalostinsnow@gmail.com>
2017-01-16 17:49:56 +0100

    Fixed threads

--- lab1/main.c
+++ lab1/main.c
@@ -3,15 +3,15 @@
 #include <unistd.h>
 
 void* print_moon(){
-    int n = 0;
+
     while(1){
-        printf("Hello moon %d\n", n);
-	n++;
+        printf("Hello moon \n", n);
+
         usleep(200);
     }
 }
 
-int Main(void) {
+int main(void) {
 	int i;
     pthread_t thread;
     pthread_create(&thread, NULL, print_moon, NULL);
