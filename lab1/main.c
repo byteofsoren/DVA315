f8a04eb88180725afc5f2db56ceb313c0fd5a6db
byteofsoren <r00tr4t@gmail.com>
2017-01-16 16:43:46 +0100

    Added new function

--- lab1/main.c
+++ lab1/main.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void print_moon(){
+    while(1){
+        printf("Heloo moon");
+    }
+}
 
-
-int main(void) {
+int Main(void) {
 	int i;
+    pthread_t thread;
 	for(i = 0; i < 10; i++) {
 		printf("Hello world!\n");
 		sleep(1);
