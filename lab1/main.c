34c7e29a2aae86208fc88c3588f122dfffa4faeb
byteofsoren <r00tr4t@gmail.com>
2017-01-16 17:02:16 +0100

    Added threeds

--- lab1/main.c
+++ lab1/main.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void print_moon(){
+void* print_moon(){
     while(1){
         printf("Heloo moon");
+        sleep(1);
     }
 }
 
 int Main(void) {
 	int i;
     pthread_t thread;
+    pthread_create(&thread, NULL, print_moon, NULL);
 	for(i = 0; i < 10; i++) {
 		printf("Hello world!\n");
 		sleep(1);
