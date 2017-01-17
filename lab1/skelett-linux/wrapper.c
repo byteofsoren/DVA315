b556b8cec7ef0827d98baa00ea03c77e557b877c
byteofsoren <r00tr4t@gmail.com>
2017-01-18 00:54:26 +0100

    Deleted blank lines

--- lab1/skelett-linux/wrapper.c
+++ lab1/skelett-linux/wrapper.c
@@ -1,11 +1,8 @@
 #include "wrapper.h"
-
 #define MAX_SIZE 1024
 
 int MQcreate (mqd_t * mq, char * name)
 {
-    
-
 }
 int MQconnect (mqd_t * mq, char * name)
 {
@@ -13,21 +10,17 @@ int MQconnect (mqd_t * mq, char * name)
 
 int MQread (mqd_t * mq, char ** refBuffer)
 {
-    
 }
 
 int MQwrite (mqd_t * mq, char * sendBuffer)
 {
-    
 }
 
 int MQclose(mqd_t * mq, char * name)
 {
-    
 }
 int threadCreate (void * functionCall, int threadParam)
 {
-	
 }
 
 
