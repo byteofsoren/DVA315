794139d123c257e21a9cd6a8bb34b33fa8b6ae49
byteofsoren <r00tr4t@gmail.com>
2017-01-20 13:37:29 +0100

    added input

---
+++ lab1/input.h
@@ -0,0 +1,13 @@
+// Prototype def
+
+#ifndef TUNA_INPUT
+#define TUNA_INPUT
+
+extern void *input(const char *prompt);
+// usage 
+//  char* text;
+//  text = (char*)input("promtmpt");
+//  printf("text was: %s" , text);
+//  free(text);
+
+#endif
