15c2f89bed9e40290d02bccad2364c32ec98ee6f
byteofsoren <r00tr4t@gmail.com>
2017-01-20 16:20:46 +0100

    added commet from teacher

--- lab1/wrapper.c
+++ lab1/wrapper.c
@@ -4,6 +4,9 @@
 
 int MQcreate (mqd_t * mq, char * name)
 {
+    /* Creates a mailslot with the specified name. Uses mq as reference
+     * pointer, so that you can     reach the handle from anywhere */    /*
+      Should be able to handle a messages of any size */    /* Should return 1 on success and 0 on fail*/
     struct mq_attr attr;
     attr.mq_flags = 0;
     attr.mq_maxmsg = 10;
@@ -14,6 +17,8 @@ int MQcreate (mqd_t * mq, char * name)
 }
 int MQconnect (mqd_t * mq, char * name)
 {
+    /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can 	reach the handle from anywhere*/
+    /* Should return 1 on success and 0 on fail*/
     struct mq_attr attr;
      attr.mq_flags = 0;
      attr.mq_maxmsg = 10;
@@ -25,21 +30,34 @@ int MQconnect (mqd_t * mq, char * name)
 
 int MQread (mqd_t * mq, char ** refBuffer)
 {
+    /* Read a msg from a mailslot, return nr Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
+    /* of successful bytes read              */
+
     return mq_receive(*mq, *refBuffer, MAX_SIZE, 0);
 }
 
 int MQwrite (mqd_t * mq, char * sendBuffer)
-{   
+{
+    /* Write a msg to a mailslot, return nr Uses mq as reference pointer, so that you can 	     reach the handle from anywhere*/
+    /* of successful bytes written         */
+
     return mq_send(*mq, sendBuffer, MAX_SIZE, 0);
 
 }
 
 int MQclose(mqd_t * mq, char * name)
 {
- return mq_close(*mq);
+    /* close a mailslot, returning whatever the service call returns Uses mq as reference pointer, so that you can 	
+    reach the handle from anywhere*/
+    /* Should return 1 on success and 0 on fail*/
+
+	 return mq_close(*mq);
 }
 int threadCreate (void * functionCall, int threadParam)
 {
+	/* Creates a thread running threadFunc */
+	/* Should return 1 on success and 0 on fail*/
+
 	pthread_t thread;
     int* par = (void *)calloc(sizeof(int), 1);
     *par = threadParam;  
