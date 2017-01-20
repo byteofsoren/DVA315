b880235cdccb08eca353064a87218a4b08c18d52
byteofsoren <r00tr4t@gmail.com>
2017-01-20 16:40:46 +0100

    wrapper fixed formating and white spaces

--- lab1/wrapper.c
+++ lab1/wrapper.c
@@ -5,8 +5,9 @@
 int MQcreate (mqd_t * mq, char * name)
 {
     /* Creates a mailslot with the specified name. Uses mq as reference
-     * pointer, so that you can     reach the handle from anywhere */    /*
-      Should be able to handle a messages of any size */    /* Should return 1 on success and 0 on fail*/
+     * pointer, so that you can reach the handle from anywhere
+     * Should be able to handle a messages of any size
+     * Should return 1 on success and 0 on fail*/
     struct mq_attr attr;
     attr.mq_flags = 0;
     attr.mq_maxmsg = 10;
@@ -17,9 +18,10 @@ int MQcreate (mqd_t * mq, char * name)
 }
 int MQconnect (mqd_t * mq, char * name)
 {
-    /* Connects to an existing mailslot for writing Uses mq as reference pointer, so that you can 	reach the handle from anywhere*/
-    /* Should return 1 on success and 0 on fail*/
-    struct mq_attr attr;
+    /* Connects to an existing mailslot for writing Uses mq as reference pointer,
+     * so that you can 	reach the handle from anywhere
+     * Should return 1 on success and 0 on fail*/
+     struct mq_attr attr;
      attr.mq_flags = 0;
      attr.mq_maxmsg = 10;
      attr.mq_msgsize = MAX_SIZE;
@@ -30,16 +32,18 @@ int MQconnect (mqd_t * mq, char * name)
 
 int MQread (mqd_t * mq, char ** refBuffer)
 {
-    /* Read a msg from a mailslot, return nr Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
-    /* of successful bytes read              */
+    /* Read a msg from a mailslot, return nr Uses mq as reference pointer,
+     * so that you can 	reach the handle from anywhere
+     * of successful bytes read */
 
     return mq_receive(*mq, *refBuffer, MAX_SIZE, 0);
 }
 
 int MQwrite (mqd_t * mq, char * sendBuffer)
 {
-    /* Write a msg to a mailslot, return nr Uses mq as reference pointer, so that you can 	     reach the handle from anywhere*/
-    /* of successful bytes written         */
+    /* Write a msg to a mailslot, return nr Uses mq as reference pointer,
+     * so that you can reach the handle from anywhere
+    * of successful bytes written */
 
     return mq_send(*mq, sendBuffer, MAX_SIZE, 0);
 
@@ -47,9 +51,10 @@ int MQwrite (mqd_t * mq, char * sendBuffer)
 
 int MQclose(mqd_t * mq, char * name)
 {
-    /* close a mailslot, returning whatever the service call returns Uses mq as reference pointer, so that you can 	
-    reach the handle from anywhere*/
-    /* Should return 1 on success and 0 on fail*/
+    /* close a mailslot, returning whatever the service call returns
+     * Uses mq as reference pointer, so that you can
+     * reach the handle from anywhere
+    * Should return 1 on success and 0 on fail*/
 
 	 return mq_close(*mq);
 }
@@ -60,7 +65,7 @@ int threadCreate (void * functionCall, int threadParam)
 
 	pthread_t thread;
     int* par = (void *)calloc(sizeof(int), 1);
-    *par = threadParam;  
+    *par = threadParam;
 	return pthread_create(&thread, NULL, functionCall, par);
 }
 
