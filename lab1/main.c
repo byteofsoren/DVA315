acbab8d716247e1dd9b28df25b8ecbcfe78b2e24
green4free <ninjalostinsnow@gmail.com>
2017-01-25 16:17:35 +0100

    Works, but not 100% of the time

--- lab1/main.c
+++ lab1/main.c
@@ -2,29 +2,24 @@
 #include <unistd.h>
 #include "wrapper.h"
 #include "input.h"
-#define NAME "/test"
+#define NAME "test"
 #define second 1000000
 pthread_mutex_t p;
 
 
 
 
-<<<<<<< HEAD
+
 void* print_moon(void * arg){
     struct messageBuffer buf;
     buf.mtype = 1;
     int id;
-    MQcreate(&id, NAME);   
-=======
-void* print_moon(void){
-    mqd_t talker;
-    MQconnect(&talker, name);
-    char * data;
->>>>>>> 2b3273095f24148776cef1b86a714072220eb829
     while(1){
         pthread_mutex_lock(&p);
+        MQcreate(&id, NAME);
         MQread(id, 1, &buf);
         printf("%s\n", buf.data);
+        MQclose(id);
         pthread_mutex_unlock(&p);
         usleep(100);
     }
@@ -35,22 +30,15 @@ int main(void) {
     struct messageBuffer buf;
     buf.mtype = 1;
     int id;
-    MQcreate(&id, NAME);
     threadCreate(print_moon, 0);
     buf.data = (char*)calloc(1,1);
     while(1){
         pthread_mutex_lock(&p);
-<<<<<<< HEAD
+        MQcreate(&id, NAME);
         free(buf.data);
         buf.data = (char*)input("prompt: ");
         MQwrite(id, &buf);
-=======
-        char* text;
-        text = (char*)input("prompt: ");
-        MQwrite(&talker, text);
->>>>>>> 2b3273095f24148776cef1b86a714072220eb829
         pthread_mutex_unlock(&p);
-        free(text);
         usleep(100);
 	}
 	return 0;
--- lab1/wrapper.c
+++ lab1/wrapper.c
@@ -5,48 +5,19 @@
 
 int MQcreate (int *id, char * name)
 {
-<<<<<<< HEAD
     key_t mq = (key_t)ftok(name, 'm');
     *id = msgget(mq, 0666 | IPC_CREAT);
     if (*id == -1) return 0;
     return 1;
-=======
-    /* Creates a mailslot with the specified name. Uses mq as reference
-     * pointer, so that you can reach the handle from anywhere
-     * Should be able to handle a messages of any size
-     * Should return 1 on success and 0 on fail*/
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = MAX_SIZE;
-    attr.mq_curmsgs = 0;
-    *mq = mq_open(name, O_CREAT | O_RDWR, &attr);
-    return (mqd_t)-1 != *mq;
->>>>>>> 2b3273095f24148776cef1b86a714072220eb829
 }
 
 int MQconnect (int *id, char * name)
 {
-<<<<<<< HEAD
     return MQcreate(id, name);
-=======
-    /* Connects to an existing mailslot for writing Uses mq as reference pointer,
-     * so that you can 	reach the handle from anywhere
-     * Should return 1 on success and 0 on fail*/
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = MAX_SIZE;
-    attr.mq_curmsgs = 0;
-    *mq = mq_open(name, O_RDWR, &attr);
-    //perror("mq_open");
-    return (mqd_t)-1 != *mq;
->>>>>>> 2b3273095f24148776cef1b86a714072220eb829
 }
 
 int MQread (int id, long type, struct messageBuffer *dataBuffer)
 {
-<<<<<<< HEAD
     int     result, length;
     /* The length is essentially the size of the structure minus sizeof(mtype) */
     length = sizeof(struct messageBuffer) - sizeof(long);
@@ -77,37 +48,6 @@ int MQclose(int id)
         return(-1);
     }        
     return(0);
-=======
-    /* Read a msg from a mailslot, return nr Uses mq as reference pointer,
-     * so that you can 	reach the handle from anywhere
-     * of successful bytes read */
-
-    int ret =  mq_receive(*mq, *refBuffer, MAX_SIZE, 0);
-    perror("mx_receive");
-    return ret;
-}
-
-int MQwrite (mqd_t * mq, char * sendBuffer)
-{
-    /* Write a msg to a mailslot, return nr Uses mq as reference pointer,
-     * so that you can reach the handle from anywhere
-    * of successful bytes written */
-
-    int ret = mq_send(*mq, sendBuffer, MAX_SIZE, 0);
-    perror("mq_send");
-    return ret;
-
-}
-
-int MQclose(mqd_t * mq)
-{
-    /* close a mailslot, returning whatever the service call returns
-     * Uses mq as reference pointer, so that you can
-     * reach the handle from anywhere
-    * Should return 1 on success and 0 on fail*/
-
-	 return mq_close(*mq);
->>>>>>> 2b3273095f24148776cef1b86a714072220eb829
 }
 
 int threadCreate (void * functionCall, int threadParam)
