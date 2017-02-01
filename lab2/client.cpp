f1afa2491353c507ba46483e6eee44c073bfe6f3
byteofsoren <r00tr4t@gmail.com>
2017-02-01 08:34:57 +0100

    Started labb2

---
+++ lab2/client.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include "wrapper.h"
+
+#define PLANETS 1000000
+#define PLANETIPC "PlanetLab"
+mqd_t serverHandle;
+int connectToServer(char * name)
+{
+	int errorCode = 0;
+	//Create a socket to handle communication with client
+	errorCode = MQconnect (&serverHandle, name);
+	return errorCode;
+}
+
+
+int main()
+{
+	//Instead of using a returnhandle, we instead use a global mailslot handle (serverHandle)
+	//I think this is more convenient, however, would you could alter the method so that it
+	//returns a handle
+	int ec = 0; 
+	if ((ec = connectToServer(PLANETIPC))==0)
+	{
+		printf("Failed to connect to server!");
+	}
+	
+	while(1)
+	{
+
+		//Obviously, since you have already made mailslot read compatible with the
+		//Planet struct, you should send a planet to the server
+
+		//Instructions as following:
+		//Initialize a planet struct
+		//Send the planet struct to the server using MQwrite(&serverHandle)
+		//if write is success, then you are very well on your way to a planet system 
+	}
+	if ((ec = MQclose(&serverHandle, PLANETIPC))==0)
+	{
+		printf("Failed to close server, watch out for memory leaks");
+	}
+	return 0;
+}
---
+++ lab2/server.cpp
@@ -0,0 +1,47 @@
+
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+#include "wrapper.h"
+
+
+#define FRAMEUPDATETIMER 10000 //Microseconds, increase value for slower update time
+
+void mailThread()
+{
+	//Mailing thread
+}
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "Orbital system");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    float x = 0.0;
+    float y = 0.0;
+
+    while (window.isOpen())
+    {
+	//Dont remove!
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+	//Dont remove!
+	shape.setPosition(x, y);
+        window.clear();
+        window.draw(shape);
+        window.display();
+	x++;
+	y++;
+	usleep(FRAMEUPDATETIMER);
+	fflush(stdout);
+	printf("Debug");
+    }
+
+
+    return 0;
+}
